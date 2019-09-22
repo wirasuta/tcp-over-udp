ed63023c4d60de8d6e03759530f4707054c804fc
I Putu Gede Wirasuta <wirasutat@gmail.com>
2019-09-22 11:13:50 +0700

    implement single packet send with dummy receiver

--- receiver.py
+++ receiver.py
@@ -6,10 +6,22 @@ from packet import Packet
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 13337         # Port to listen on (non-privileged ports are > 1023)
+MAX_PACKET_SIZE = 33000
+
+# TODO: Check for packet completeness
+# TODO: File sequencing and sequencing
 
 
 def handler(sock, addr, data):
-    print(Packet.from_bytes(data))
+    recv_packet = Packet.from_bytes(data)
+    print(f'{addr} -> {str(recv_packet)}')
+
+    reply_type = 'ACK' if recv_packet.get_type() == 'DATA' else 'FIN-ACK'
+    packet_id = recv_packet.id
+
+    reply_packet = Packet(reply_type, packet_id, 0, 0, '')
+    sock.sendto(reply_packet.to_bytes(), addr)
+    print(f'{addr} <- {str(reply_packet)}')
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -18,8 +30,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     print(f'Listening on port {PORT}')
 
     while True:
-        data, addr = s.recvfrom(1024)
-        print(addr)
+        data, addr = s.recvfrom(MAX_PACKET_SIZE)
         conn_thread = threading.Thread(target=handler, args=(s, addr, data))
-        conn_thread.setDaemon(True)
         conn_thread.start()
--- sender.py
+++ sender.py
@@ -1,13 +1,84 @@
 #!/usr/bin/env python3
 
 import socket
+from threading import Thread, Event, Lock
 from packet import Packet
 
 DEST_HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 DEST_PORT = 13337        # Port to listen on (non-privileged ports are > 1023)
-MESSAGE = b'A' * 16
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+HOST = '127.0.0.1'
+PORT = 14045
+
+MAX_PACKET_SIZE = 33000
+MESSAGE = b'A' * 16
+MESSAGE2 = b'B' * 32
+MESSAGE3 = b'C' * 32768
+
+
+class TCPSendThread(Thread):
+    def __init__(self, packets, dest, event):
+        Thread.__init__(self)
+        self.stopped = event
+        self.dest = dest
+        self.unacknowledged_packets = packets
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.bind((HOST, PORT))
+            ack_stop_flag = Event()
+            ack_thread = TCPAckThread(
+                self.unacknowledged_packets, sock, ack_stop_flag)
+            ack_thread.start()
+
+            for unacknowledged_packet in self.unacknowledged_packets:
+                sock.sendto(unacknowledged_packet.to_bytes(), self.dest)
+                print(f'{self.dest} <- {unacknowledged_packet}')
+
+            while not self.stopped.wait(1):
+                if len(self.unacknowledged_packets) == 0:
+                    ack_stop_flag.set()
+                    self.stopped.set()
+                for unacknowledged_packet in self.unacknowledged_packets:
+                    sock.sendto(
+                        unacknowledged_packet.to_bytes(), self.dest)
+                    print(f'Retrying to send {unacknowledged_packet}')
+
+            ack_thread.join()
+
+        print(f'All package sent!')
+
+
+class TCPAckThread(Thread):
+    def __init__(self, unacknowledged_packets, sock, event):
+        Thread.__init__(self)
+        self.stopped = event
+        self.unacknowledged_packets = unacknowledged_packets
+        self.sock = sock
+
+    def run(self):
+        print(f'Listening on port {PORT}')
+
+        while not self.stopped.is_set():
+            data, addr = self.sock.recvfrom(MAX_PACKET_SIZE)
+            ack_packet = Packet.from_bytes(data)
+            print(f'{addr} -> {ack_packet}')
+
+            self.unacknowledged_packets.remove(ack_packet)
+            if (ack_packet.get_type() == 'FIN-ACK'):
+                self.stopped.set()
+
+        print(f'All package acknowledged')
+
+
+if __name__ == '__main__':
     pack1 = Packet('DATA', Packet.pick_id(), 0, len(MESSAGE), MESSAGE)
-    print(pack1)
-    s.sendto(pack1.to_bytes(), (DEST_HOST, DEST_PORT))
+    pack2 = Packet('DATA', Packet.pick_id(), 0, len(MESSAGE2), MESSAGE2)
+    pack3 = Packet('FIN', Packet.pick_id(), 0, len(MESSAGE3), MESSAGE3)
+    packets = [pack1, pack2, pack3]
+    dest = (DEST_HOST, DEST_PORT)
+    stop_flag = Event()
+
+    send_thread = TCPSendThread(packets, dest, stop_flag)
+    send_thread.start()
+    send_thread.join()
